8ec9f6918b020a6b709f4dca6a7cd92829bde822
Kelvonish <kelvomyself@gmail.com>
2021-06-14 17:13:11 +0300

    code clean up and complleted campaigns code

--- lib/presentation/screens/test.dart
+++ lib/presentation/screens/test.dart
@@ -3,36 +3,3 @@
 // found in the LICENSE file.
 
 // ignore_for_file: public_member_api_docs
-
-import 'package:flutter/material.dart';
-
-import 'package:share_plus/share_plus.dart';
-
-//import 'image_previews.dart';
-
-class DemoApp extends StatefulWidget {
-  final String id;
-  DemoApp({this.id});
-  @override
-  DemoAppState createState() => DemoAppState();
-}
-
-class DemoAppState extends State<DemoApp> {
-  String text = '';
-  String subject = '';
-  List<String> imagePaths = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Share Plugin Demo',
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Share Plugin Demo'),
-          ),
-          body: Container(
-            child: Text("rightPlace"),
-          )),
-    );
-  }
-}
